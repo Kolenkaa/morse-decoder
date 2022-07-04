0bacddf48b363137889e456026bee3807521078d
Kolenkaa <kkolenkaa@gmail.com>
2022-07-05 01:51:30 +0300

    add solution for morse decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+	let arr = [];
+	let result = [];
+	let chunk = expr.length / 10;
+ 
+	for (let i = 0; i < chunk; i++) {
+	  let subStr = expr.slice(i * 10, i * 10 + 10);
+	  arr.push(subStr);
+	}
+	for (let i = 0; i < arr.length; i++) {
+	  if (arr[i] === "**********") {
+		 result.push(' ');
+	  } else {
+		 arr[i] = arr[i].replace(/10/g, '.').replace(/11/g, '-').replace(/00/g, '');
+		 result.push(MORSE_TABLE[arr[i]]);
+	  }
+	}
+	return result.join('');
+ }
 
 module.exports = {
     decode
